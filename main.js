63257779a5516a16c681d4d5b089bcd6d58f1ef8
arya askarpour <ayaska@pm.me>
2023-08-13 17:53:02 +0100

    standardjs formatter

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-function getComputerChoice() {
-    choices = ["Rock", "Paper", "Scissors"];
-    return choices[Math.floor(Math.random() * choices.length)]
+function getComputerChoice () {
+  const choices = ['Rock', 'Paper', 'Scissors']
+  return choices[Math.floor(Math.random() * choices.length)]
 }
 
-console.log(`Computer Choice: ${getComputerChoice()}`);
+console.log(`Computer Choice: ${getComputerChoice()}`)
 
 // returns a titlecase formatted choice
-function normalizeUserChoice(choice) {
-    return choice.slice(0, 1).toUpperCase() + choice.slice(1).toLowerCase();
+function normalizeUserChoice (choice) {
+  return choice.slice(0, 1).toUpperCase() + choice.slice(1).toLowerCase()
 }
 
 // returns result string: does a beat b?
-function playRound(playerChoice, computerChoice) {
-    // validity checks
-    let validChoices = ["Rock", "Paper", "Scissors"]
-    if (!["Rock", "Paper", "Scissors"].includes(playerChoice)) {
-        return "Invalid choice from Player! Try again"
-    }
+function playRound (playerChoice, computerChoice) {
+  // validity checks
+  const validChoices = ['Rock', 'Paper', 'Scissors']
+  if (!validChoices.includes(playerChoice)) {
+    return 'Invalid choice from Player! Try again'
+  }
 
-    if (!["Rock", "Paper", "Scissors"].includes(computerChoice)) {
-        return "Invalid choice from Computer! Try again"
-    }
+  if (!validChoices.includes(computerChoice)) {
+    return 'Invalid choice from Computer! Try again'
+  }
 
-    // check for tie
-    if (playerChoice === computerChoice) {
-        return `Tie! You both picked ${playerChoice}.`
-    }
+  // check for tie
+  if (playerChoice === computerChoice) {
+    return `Tie! You both picked ${playerChoice}.`
+  }
 
-    // decide if player won
-    let playerWon;
+  // decide if player won
+  let playerWon
 
-    if (playerChoice === "Rock") {
-        playerWon = (computerChoice === "Paper") // true if paper, false otherwise
-    } else if (playerChoice === "Paper") {
-        playerWon = (computerChoice === "Rock")
-    } else {
-        playerWon = (computerChoice === "Scissors")
-    }
+  if (playerChoice === 'Rock') {
+    playerWon = (computerChoice === 'Paper') // true if paper, false otherwise
+  } else if (playerChoice === 'Paper') {
+    playerWon = (computerChoice === 'Rock')
+  } else {
+    playerWon = (computerChoice === 'Scissors')
+  }
 
-    // return result
-    if (playerWon) {
-        return `You win! ${playerChoice} beats ${computerChoice}`;
-    } else {
-        return `You lose! ${computerChoice} beats ${playerChoice}`;
-    }
+  // return result
+  if (playerWon) {
+    return `You win! ${playerChoice} beats ${computerChoice}`
+  } else {
+    return `You lose! ${computerChoice} beats ${playerChoice}`
+  }
 }
